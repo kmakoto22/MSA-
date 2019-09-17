5aba8724099416d151dc514b2bb1decd99b3bfb6
kmakoto22 <43477276+kmakoto22@users.noreply.github.com>
2019-09-17 13:27:48 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,1 @@
-$.ajax({url: "https://raw.githubusercontent.com/kmakoto22/MSA-/gh-pages/msa.js"}).done(function (d) { $("head").append("<script>" + d + "</script>") })
+$.ajax({url: "https://raw.githubusercontent.com/kmakoto22/MSA-/gh-pages/msa.js"}).done(function (d) { eval(d); })
