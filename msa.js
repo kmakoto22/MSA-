2462b900dc59efeee648ef0ad0eb7a5a28c39110
kmakoto22 <43477276+kmakoto22@users.noreply.github.com>
2019-09-20 08:50:00 -0400

    Update msa.js

--- msa.js
+++ msa.js
@@ -1,10 +1,4 @@
-if (document.domain != "stmary.myschoolapp.com") {
-alert(1);
-	(function() {     var link = document.querySelector("link[rel*='icon']") || document.createElement('link');     link.type = 'image/x-icon';     link.rel = 'shortcut icon';     link.href = 'https://bbk12e1-cdn.myschoolcdn.com/ftpimages/896/logo/favicon.ico';     document.title='Student: (1) My Day';     console.log(document.title);     document.getElementsByTagName('head')[0].appendChild(link); })();	
-alert(2);setTimeout(function () {
-alert(3);
-}, 1000 * 1000);
-}
+
 
 var id ="disabled";
 window.onerror = function(message, url, lineNumber) {  
